9f64174ac95c860dc4ff522c964af9609370df8e
Benson Njuguna <njugunab655@gmail.com>
2022-07-19 11:01:35 +0300

    made additions to functional programming and added a new div

--- index.js
+++ index.js
@@ -1,32 +1,88 @@
-let searchBar = document.getElementById('search')
-
-let deleteButtons = document.getElementsByClassName('delete-button'); 
-
-let header = document.getElementsByClassName('header-container')
-
-let addTaskButton = document.getElementById('add-task')
-
-let addTaskBox = document.getElementById('add-task-box')
-
-let editButtons = document.getElementsByClassName('color')
-
-let lis = document.getElementsByTagName('li') //this gives a html collection and not a normal array
-
-//Turn the HTML collection to an array 
-let lisArr = Array.from(lis)
-
-let addedTasksContainer = document.querySelector('.task-container')
-
-//wordings added as tasks
-let taskNames = document.getElementsByClassName('name')
+//delete a task
+const list = document.querySelector('.task-container ul'); 
+list.addEventListener('click', function(e){
+    if(e.target.className == 'delete-button'){
+        const li = e.target.parentElement;
+        list.removeChild(li);
+    }
+})
 
 
-//convert the taskNames into an array
-let taskNamesArr = Array.from(taskNames)
+//add tasks to the list
+const addForm = document.forms['task-entry'];
+addForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    const value = addForm.querySelector('input[type="text"]').value;
 
-// add a task number to each task
-for (let i =0; i < taskNamesArr.length; i++) {
-    taskNamesArr[i].textContent += ` (Task ${i + 1})`
-}
+    //create elements
+    const li = document.createElement('li');
+    const thumbtack = document.createElement('i');
+    const taskName = document.createElement('span');
+    const deleteButton = document.createElement('button');
+    const editIcon = document.createElement('i');
+
+    //add classes 
+    taskName.classList.add('name');
+    deleteButton.classList.add('delete-button');
+    editIcon.classList.add('fa-solid', 'fa-pen-to-square', 'color');
+    thumbtack.classList.add('fa-solid', 'fa-thumbtack');
+    
+    // thumbtack.classList.add('fa-solid fa-thumbtack')
+    // taskName.classList.add('name');
+    // deleteButton.classList.add('delete-button');
+    // editIcon.classList.add("fa-solid fa-pen-to-square color")
+
+    //append the created elements to the document
+    li.appendChild(thumbtack);
+    li.appendChild(taskName)
+    li.appendChild(deleteButton);
+    li.appendChild(editIcon);
+    list.appendChild(li);
+
+    //add content passed and the defaults
+    deleteButton.textContent = "Delete"
+    taskName.textContent = value
+
+})
+
+//setting the hide button 
+    //creating a reference to the checkbox
+    const hideTasks = document.querySelector('#hide'); 
+    hideTasks.addEventListener('change', function(e) {
+        if (hideTasks.checked) {
+            list.style.display = "none";
+        }
+        else {
+            list.style.display = "initial"
+        }
+    });
+
+const searchBar = document.forms['search-bar'].querySelector('input');
+
+searchBar.addEventListener('keyup', function(e) {
+    const term = e.target.value.toLowerCase();
+    const tasks = list.getElementsByTagName('li');
+    Array.from(tasks).forEach(function(task){
+        const description = task.querySelector('span').textContent;
+        if(description.toLowerCase().indexOf(term) != -1){
+            task.style.display = 'block'
+        }
+        else {
+            task.style.display = 'none';
+        }
+    })
+});
+
+
+
+
+// //wordings added as tasks
+// let taskNames = document.getElementsByClassName('name')
+// //convert the taskNames into an array
+// let taskNamesArr = Array.from(taskNames)
+// // add a task number to each task
+// for (let i =0; i < taskNamesArr.length; i++) {
+//     taskNamesArr[i].textContent += ` (Task ${i + 1})`
+// }
 
 
